0c77bd0ac43c0e1247eaf1d0e722a978cb681b19
Steven Manikiam <sman@manikiam.net>
2021-06-11 14:42:05 +0100

    Class for handling exceptions in our API and returning reasonable return codes and details to a client.

---
+++ src/main/java/net/manikiam/ws/peopleservice/person/boundary/exceptions/handler/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package net.manikiam.ws.peopleservice.person.boundary.exceptions.handler;
+
+import net.manikiam.ws.peopleservice.person.boundary.exceptions.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    SimpleDateFormat formatter = new SimpleDateFormat("dd MMMM yyyy HH:mm:ss");
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleObjectNotFoundException(ObjectNotFoundException exception, WebRequest request) {
+
+        ErrorDetails errorDetails = new ErrorDetails(formatter.format(new Date()), exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+}
