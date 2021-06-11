341a920ab5f13ec725320f20cbd66720cf68134f
Steven Manikiam <sman@manikiam.net>
2021-06-11 18:59:57 +0100

    catch validation exceptions and send proper response to client

--- src/main/java/net/manikiam/ws/peopleservice/person/boundary/exceptions/handler/ControllerExceptionHandler.java
+++ src/main/java/net/manikiam/ws/peopleservice/person/boundary/exceptions/handler/ControllerExceptionHandler.java
@@ -2,6 +2,9 @@ package net.manikiam.ws.peopleservice.person.boundary.exceptions.handler;
 
 import net.manikiam.ws.peopleservice.person.boundary.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,6 +30,28 @@ public class ControllerExceptionHandler {
         return errorDetails;
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ConstraintErrorDetails handleConstraintViolationException(MethodArgumentNotValidException exception, WebRequest request) {
+
+
+        ConstraintErrorDetails errorDetails = new ConstraintErrorDetails(getTimestamp(), getDetails(request));
+
+        for (ObjectError error : exception.getBindingResult().getAllErrors()) {
+
+            String objectName = error.getObjectName();
+            String message = error.getDefaultMessage();
+            String field = "N/A";
+            if (error instanceof FieldError) {
+                field = ((FieldError)error).getField();
+            }
+
+            errorDetails.addDetails(objectName, field, message);
+        }
+
+        return errorDetails;
+    }
 
     private String getDetails(WebRequest request) {
 
